747c181eed5440e6ab5541bc779bf77bd35c89c4
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2025-04-22 07:58:53 +0700

    feat: update master view with HTML structure and Bootstrap integration

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,3 +1,32 @@
-<div>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
-</div>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>Web Project</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta name="author" content="PiperBelly">
+    <meta name="description" content="Web Project">
+    <meta name="keywords" content="web, project, laravel">
+    <meta name="theme-color" content="#ffffff">
+
+    {{-- Bootstrap --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    {{-- Bttn --}}
+    <link rel="stylesheet" href="{{ asset('css/bttn.min.css') }}">
+</head>
+<body>
+    @hasSection('content')
+        @yield('content')
+    @else
+        <div class="container">
+            <h1>Welcome to the Web Project</h1>
+            <p>This is a simple web project using Laravel.</p>
+        </div>
+    @endif
+</body>
+</html>
