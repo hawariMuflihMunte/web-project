e9e866ee824864f11635144e8cde5c6f7de4c7f4
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2025-04-22 08:05:33 +0700

    feat: implement HomeController and create home view

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('master')
+
+@section('content')
+    <div class="container">
+        <h1>Welcome to the Web Project</h1>
+        <p>This is a simple web project using Laravel.</p>
+    </div>
+@endsection
