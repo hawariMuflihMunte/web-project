e79b86eabd2483f7cd7c205664a45c4c6d95ace9
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2025-04-22 11:47:05 +0700

    feat: enhance home view with Bootstrap alerts and dynamic title support

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<html lang="id">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     @hasSection('title')
-        <title>Web Project | @yield('title')</title>
+        <title>{{ config('app.name') }} | @yield('title')</title>
     @else
-        <title>Web Project</title>
+        <title>{{ config('app.name') }}</title>
     @endif
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,11 +19,11 @@
     <meta name="theme-color" content="#ffffff">
 
     {{-- Bootstrap --}}
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+    <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}" defer></script>
 
     {{-- Bttn --}}
-    <link rel="stylesheet" href="{{ asset('css/bttn.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('bttn.min.css') }}">
 </head>
 <body>
     @hasSection('content')
