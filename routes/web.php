cd719b8494f3969c5d7a68d8741782ba3e3bbbb4
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2025-04-24 06:23:04 +0700

    feat: add UpgradeInsecureRequests middleware and update routes for improved security

--- routes/web.php
+++ routes/web.php
@@ -6,35 +6,38 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\PembelajaranController;
 use App\Http\Controllers\QuizController;
+use App\Http\Middleware\UpgradeInsecureRequests;
 
-// Home
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::middleware([UpgradeInsecureRequests::class])->group(function() {
+  // Home
+  Route::get('/', [HomeController::class, 'index'])->name('home');
 
-// Auth
-Route::get('/login', [LoginController::class, 'index'])->name('login.index');
-Route::post('/login', [LoginController::class, 'store'])->name('login.store');
-Route::delete('/logout', [LoginController::class, 'destroy'])->name('login.destroy');
-Route::get('/register', [RegisterController::class, 'index'])->name('register.index');
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+  // Auth
+  Route::get('/login', [LoginController::class, 'index'])->name('login.index');
+  Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+  Route::delete('/logout', [LoginController::class, 'destroy'])->name('login.destroy');
+  Route::get('/register', [RegisterController::class, 'index'])->name('register.index');
+  Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 
-// Pembelajaran
-Route::group(['middleware' => ['role:Admin'], 'prefix' => 'admin'], function () {
-  Route::resource('pembelajaran', PembelajaranController::class);
-});
+  // Pembelajaran
+  Route::group(['middleware' => ['role:Admin'], 'prefix' => 'admin'], function () {
+    Route::resource('pembelajaran', PembelajaranController::class);
+  });
 
-Route::group(['middleware' => ['role:Siswa'], 'prefix' => 'siswa'], function () {
+  Route::group(['middleware' => ['role:Siswa'], 'prefix' => 'siswa'], function () {
+    Route::resource('pembelajaran', PembelajaranController::class)->only('index', 'show');
+  });
+
   Route::resource('pembelajaran', PembelajaranController::class)->only('index', 'show');
-});
 
-Route::resource('pembelajaran', PembelajaranController::class)->only('index', 'show');
+  // Quiz
+  Route::group(['middleware' => ['role:Admin'], 'prefix' => 'admin'], function () {
+    Route::resource('quiz', QuizController::class);
+  });
 
-// Quiz
-Route::group(['middleware' => ['role:Admin'], 'prefix' => 'admin'], function () {
-  Route::resource('quiz', QuizController::class);
-});
+  Route::group(['middleware' => ['role:Siswa'], 'prefix' => 'siswa'], function () {
+    Route::resource('quiz', QuizController::class)->only('index', 'show', 'edit');
+  });
 
-Route::group(['middleware' => ['role:Siswa'], 'prefix' => 'siswa'], function () {
   Route::resource('quiz', QuizController::class)->only('index', 'show', 'edit');
 });
-
-Route::resource('quiz', QuizController::class)->only('index', 'show', 'edit');
