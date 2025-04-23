58eff6787d0d402397e4cda8f11d1d997d41abba
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2025-04-23 14:11:51 +0700

    refactor: remove unused data from KuisSeeder for cleaner code

--- database/seeders/KuisSeeder.php
+++ database/seeders/KuisSeeder.php
@@ -6,21 +6,6 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Pembelajaran;
 use App\Models\Quiz;
-$pembelajarans = array(
-	array(
-		"id" => 1,
-		"slug" => "cerita-pendek",
-		"judul" => "Cerita Pendek",
-		"deskripsi" => "Pada modul ajar di topik “ Cerita Pendek” membahas mengenai kebudayaan daerah yang dituangkan dalam bentuk cerita-cerita pendek yang harapannya untuk membukakan gagasan terhadap keilmuan sastra daerah yang dapat lebih dikenalkan dan dimaknai lebih sederhana karena berbentuk cerita pendek yang menarik.",
-		"tujuan_pembelajaran" => "Meningkatkan Motivasi.;; Meningkatkan Pengetahuan.;; Meningkatkan Keterampilan.",
-		"materi_tambahan" => NULL,
-		"lampiran" => NULL,
-		"gambar" => NULL,
-		"created_by" => "64e1b8d3",
-		"created_at" => "2025-04-23 02:56:19",
-		"updated_at" => "2025-04-23 02:56:19",
-	),
-);
 
 class KuisSeeder extends Seeder
 {
