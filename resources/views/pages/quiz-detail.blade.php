f40f29e1aa484179ab8d3f1bbb7a302765bfd799
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2025-04-23 15:26:21 +0700

    feat: enhance quiz detail view with improved layout and additional information sections

--- resources/views/pages/quiz-detail.blade.php
+++ resources/views/pages/quiz-detail.blade.php
@@ -3,8 +3,52 @@
 @section('title', 'Modul '.$quiz->pembelajaran->judul.', '.$quiz->judul)
 @section('content')
     @include('layouts.navbar')
-    <section class="container py-5">
-        {{ $quiz }}
+    <section class="container py-5 my-5">
+        <div class="container">
+            <h1>{{ $quiz->judul }}</h1>
+            <section class="w-100">
+                @if ($quiz->gambar)
+                    <img src="{{ asset($quiz->gambar) }}" alt="" class="img-fluid">
+                @else
+                    <img src="{{ asset('images/undraw_preparation_59f0.png') }}" alt="A man and a woman preparing for examination." class="img-fluid">
+                @endif
+            </section>
+            <section class="py-3">
+                <h2 class="mb-3">Deskripsi</h2>
+                <p style="max-width: 60ch;">{{ $quiz->deskripsi }}</p>
+            </section>
+            <section class="py-3">
+                <h2 class="mb-3">Informasi</h2>
+                <section class="d-flex flex-column justify-content-center align-items-start gap-3">
+                    <section class="bg-light p-3 rounded-3">
+                        <i class="bi bi-hourglass-split"></i>
+                        <span>0 menit</span>
+                    </section>
+                    <section class="bg-light p-3 rounded-3">
+                        <i class="bi bi-check-circle"></i>
+                        <span>0 soal</span>
+                    </section>
+                </section>
+            </section>
+            <section class="py-3">
+                <h2 class="mb-3">Instruksi</h2>
+                <ul class="list-unstyled bg-light p-4 rounded-3" style="width: max-content;">
+                    <li class=""><i class="bi bi-asterisk" style="font-size: 10px;"></i> Kerjakan soal dengan benar, jujur, teliti, dan baik.</li>
+                    <li class="mt-2"><i class="bi bi-asterisk" style="font-size: 10px;"></i> Pilih satu jawaban yang paling tepat dari opsi yang tersedia.</li>
+                    <li class="mt-2"><i class="bi bi-asterisk" style="font-size: 10px;"></i> Waktu akan berjalan otomatis saat quiz dimulai.</li>
+                    <li class="mt-2"><i class="bi bi-asterisk" style="font-size: 10px;"></i> Quiz akan berakhir secara otomatis saat waktu habis.</li>
+                    <li class="mt-2"><i class="bi bi-asterisk" style="font-size: 10px;"></i> Pastikan koneksi internet Anda stabil selama mengerjakan.</li>
+                </ul>
+            </section>
+            <section class="py-3 row">
+                <section class="col-12 col-lg-8 col-xl-8 col-xxl-8">
+                    <a href="{{ route('pembelajaran.show', $quiz->pembelajaran->slug) }}" class="btn btn-outline-secondary btn-md w-100"><i class="bi bi-arrow-left-short"></i> Kembali</a>
+                </section>
+                <section class="col-12 col-lg-4 col-xl-4 col-xxl-4 mt-3 mt-lg-0 mt-xl-0 mt-xxl-0">
+                    <a href="#" class="btn bttn-unite bttn-md w-100">Mulai</a>
+                </section>
+            </section>
+        </div>
     </section>
     @include('layouts.footer')
 @endsection
